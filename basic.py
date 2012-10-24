9eb4a83b7d0186bff4a7364711676be8bcd10d45
drew verlee <drew.verlee@gmail.com>
2012-10-24 19:34:57 -0400

    basic - cleaned up width,axes font,etc

--- basic.py
+++ basic.py
@@ -1,8 +1,9 @@
-from matplotlib.pyplot import figure, show, xticks
-import numpy as np
+from matplotlib.pyplot import figure, show
+import matplotlib.cm as cm
 from math import pi
 from random import randrange as rr 
-fig = figure(figsize=(14,14))
+import numpy as np
+
 labels = ["PC Team wellness", "Interdisciplinary Team", "Coverage/Ability",
         "Standard Form", "Initial PC evaluation", "Hospital PC screening",
         "PC follow up", "Post Discharge Services", "Bereavement Contacts",
@@ -10,14 +11,24 @@ labels = ["PC Team wellness", "Interdisciplinary Team", "Coverage/Ability",
         "Availability", "Family Centerdness", "PC Networking", 
         "Education and training", "Certification"]
 
-ax = fig.add_subplot(111, polar=True)
+fig = figure(figsize=(12,12)) 
+ax = fig.add_axes([0.1, 0.1, 0.8, 0.8], polar=True)
+
+
+#ax = fig.add_subplot(111, polar=True)
 deg = [360/len(labels) * x for x in range(1,len(labels)+1)]
-radii = [i*pi/180 for i in deg]  # convert to radians
+theta = [i*pi/180 for i in deg]  # convert to radians
 
-height = [rr(0,6) for x in range(len(labels))]
-ax.bar(radii,height, width=0.2, align='center')
+radii = [rr(0,6) for x in range(len(labels))]
+N = len(labels)
+#width = np.pi/7*np.random.rand(N)
+#TODO figure out width... artistically
+bars = ax.bar(theta,radii, width=0.35, bottom=0.0, align='center')
+for r,bar in zip(radii, bars):
+    bar.set_facecolor( cm.jet(r/10.))
+    bar.set_alpha(0.5)
 
-ax.set_thetagrids(deg,labels)
+ax.set_thetagrids(deg,labels, fontsize=15)
 
-ax.set_title("like this Cheryl?", fontsize=20)
+ax.set_title("like this Cheryl?", fontsize=50, weight="bold")
 show()
