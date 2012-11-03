01d0f08b95e495d8f771708cc8fb4d1a40fe8c12
Drew Verlee <Drew.verlee@gmail.com>
2012-11-03 15:16:19 -0400

    petal/petalapp/__init__.py, -update sqlalchemy, unsure how to configure

--- petalapp/__init__.py
+++ petalapp/__init__.py
@@ -4,6 +4,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object('defaultconfig')
 app.config.from_envvar('PETAL_DEV')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 from petalapp import views
