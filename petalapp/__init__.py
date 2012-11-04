1ed4b2d258cd248a0878e7a23f00a3e067418d5c
Drew Verlee <Drew.verlee@gmail.com>
2012-11-03 20:42:42 -0400

    __init__ update

--- petalapp/__init__.py
+++ petalapp/__init__.py
@@ -1,18 +1,20 @@
 from flask import Flask
 #alternative method
-#from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
+#import os
 from  flask_heroku import Heroku
-import os
+
 app = Flask(__name__)
 #configuring from my local computer
 app.config.from_object('defaultconfig')
-app.config.from_envvar('PETAL_DEV')
+app.config.from_envvar('PETAL_PRO')
 # alternative method to using flask-heroku
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-#db = SQLAlchemy(app)
+
 
 #flask heroku
 heroku = Heroku(app)
+db = SQLAlchemy(app)
 from petalapp import views
 
 
