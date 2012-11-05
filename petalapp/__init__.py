85c981e569c10bb3ff754312ee3105e11d44abe9
Drew Verlee <Drew.verlee@gmail.com>
2012-11-05 01:21:04 -0500

    __init__.py trying different config

--- petalapp/__init__.py
+++ petalapp/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 #alternative method
 from flask.ext.sqlalchemy import SQLAlchemy
-#import os
-from  flask_heroku import Heroku
+import os
+#from  flask_heroku import Heroku
 
 app = Flask(__name__)
 #configuring from my local computer
@@ -10,11 +10,11 @@ app = Flask(__name__)
 app.config.from_pyfile('config.py')
 #app.config.from_envvar('PETAL_DEV')
 # alternative method to using flask-heroku
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 #flask heroku
-heroku = Heroku(app)
-db = SQLAlchemy(app)
+#heroku = Heroku(app)
+#db = SQLAlchemy(app)
 from petalapp import views
 
