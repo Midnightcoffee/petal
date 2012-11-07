305891cbe006bed08dc05947dee724e1d2e6b999
Drew Verlee <Drew.verlee@gmail.com>
2012-11-07 05:32:41 -0500

    removed wtf upload

--- petalapp/views.py
+++ petalapp/views.py
@@ -1,9 +1,7 @@
 from flask import  make_response,render_template,url_for,request,redirect \
-,session, flash, redirect
+,session, redirect
 from petalapp import app
-from flask.ext.wtf import FileField, Form
-from tools import upload_s3_chart, download_s3_chart
-from xtools import s3_upload
+#from tools import upload_s3_chart, download_s3_chart
 #python path points to petalapp?
 from graph import plotpolar
 
@@ -50,31 +48,8 @@ def simple():
 
 @app.route("/awsgraph", methods =['GET','POST'])
 def aws():
-    destination_filename = 'pydoc.txt'
-    #TODO find out why there are imported in function, possible just import.
-    try:
-        num = int(session['number'])
-        assert (num >= 0 and num <= 10)
-    except:
-        num = 10
-    #upload_s3_chart(num,destination_filename)
-    #k = download_s3_chart(destination_filename)
-    #response = make_response(k.get_contents_to_file("/".join(
-    #    [app.config["S3_UPLOAD_DIRECTORY"],destination_filename])))
-    #response.headers['Content-Type'] = 'image/png'
-    #return render_template("")
     return render_template("awsgraph.html")
 
-class UploadForm(Form):
-    example = FileField('Example File')
-
-@app.route('/awswtf',methods=['GET','POST'])
-def upload_page():
-    form = UploadForm()
-    if form.validate_on_submit():
-        output = s3_upload(form.example)
-        flash('{src} uploaded to S3 as {dst}'.format(src=form.example.data.filename, dst=output))
-    return render_template('awswtf.html',form=form)
 
 
 
