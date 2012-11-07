ef8a1418e74690fbfbcf0f6ebf4c060001f6a54f
Drew Verlee <Drew.verlee@gmail.com>
2012-11-07 18:33:48 -0500

    petalapp/config.py -updated to handle int/str

--- petalapp/config.py
+++ petalapp/config.py
@@ -1,13 +1,13 @@
 import os
-
+#TODO: add str? to everything?
 PORT = int(os.environ.get("PORT", 5000))
 SECRET_KEY = str(os.environ.get("APP_SECRET_KEY"))
 DEBUG = str(os.environ.get("DEBUG"))
 SQLALCHEMY_DATABASE_URI=str(os.environ.get("DATABASE_URL"))
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-S3_BUCKET = os.environ.get("S3_BUCKET")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+AWS_ACCESS_KEY_ID = str(os.environ.get("AWS_ACCESS_KEY_ID"))
+S3_BUCKET = str(os.environ.get("S3_BUCKET"))
+AWS_SECRET_ACCESS_KEY = str(os.environ.get("AWS_SECRET_ACCESS_KEY"))
 TESTING = os.environ.get("TESTING", False)
-S3_UPLOAD_DIRECTORY = os.environ.get("S3_UPLOAD_DIRECTORY")
-ALLOWED_EXTENSIONS = os.environ.get("ALLOWED_EXTENSIONS")
+S3_UPLOAD_DIRECTORY = str(os.environ.get("S3_UPLOAD_DIRECTORY"))
+ALLOWED_EXTENSIONS = str(os.environ.get("ALLOWED_EXTENSIONS"))
 CSRF_ENABLED = True
