c858063319f6dfcf313d052a01fa00f7c889d728
Drew Verlee <Drew.verlee@gmail.com>
2012-11-07 18:15:36 -0500

    deleted addational heroku db, set env to heroku db

--- petalapp/users.py
+++ petalapp/users.py
@@ -1,6 +1,6 @@
 from petalapp import db
 
-class User(db.Model):
+class USER(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     email = db.Column(db.String(120), unique=True)
