3330516d6b1eb8b5f45b426e77997d121cebb1f9
Drew Verlee <Drew.verlee@gmail.com>
2012-11-06 21:16:33 -0500

    site hanging

--- petalapp/tools.py
+++ petalapp/tools.py
@@ -4,21 +4,20 @@ from petalapp import app
 
 from graph import plotpolar
 
-def upload_s3(num):
-    destination_filename = 'achart'
-    conn = boto.connect_s3(app.config["AWS_ACCESS_KEY_ID"], 
+
+def upload_s3(num,destination_filename): 
+    conn = boto.connect_s3(app.config["AWS_ACCESS_KEY_ID"],
             app.config["AWS_SECRET_ACCESS_KEY"])
     b = conn.get_bucket(app.config["S3_BUCKET"])
 
     sml = b.new_key("/".join([app.config["S3_UPLOAD_DIRECTORY"],destination_filename]))
     sml.set_contents_from_string(plotpolar(num).getvalue())
 
-# Set the file's permissions.
-    sml.set_acl(acl)
-    
-def download_s3():
-    destination_filename = 'achart'
-    conn = boto.connect_s3(app.config["AWS_ACCESS_KEY_ID"], 
+    # TODO investigate, Set the file's permissions.
+    sml.set_acl('public-read')
+
+def download_s3(destination_filename):
+    conn = boto.connect_s3(app.config["AWS_ACCESS_KEY_ID"],
             app.config["AWS_SECRET_ACCESS_KEY"])
     b = conn.get_bucket(app.config["S3_BUCKET"])
 
