46060c7ca2e07a79966dbd5f4473cfbce0435268
Drew Verlee <Drew.verlee@gmail.com>
2012-11-08 14:26:44 -0500

    petalapp/db_roles -updated with user hospital data, now to test it

--- petalapp/db_roles.py
+++ petalapp/db_roles.py
@@ -7,10 +7,38 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     email = db.Column(db.String(150), unique=True)
-    role = db.Column(db.SmallInteger, default = ROLE_USER)
+    role = db.Column(db.SmallInteger, default=ROLE_USER)
+    Hospital = db.relationship('Hospital', backref = 'name', lazy = 'dynamic')
 
     def __repr__(self):
         return '<Name %r>' % self.name
+
+class Hospital(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    User_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    Data = db.relationship('Data', backref = 'name', lazy = 'dynamic')
+
+    def __repr__(self):
+        return '<Hospital %r>' % self.name
+
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    hospital_id = db.Column(db.Integer, db.ForeignKey('Hospital.id'))
+    PC = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Pc Well %r>' % self.PC
+    #and so on...
+
+
+# labels for Categories.
+#    labels = ["PC team wellness", "Interdisciplinary Team", "Coverage/Ability",
+#        "Standard Form", "Initial PC evaluation", "Hospital PC screening",
+#        "PC follow up", "Post Discharge Services", "Bereavement Contacts",
+#        "Marking and Education","Marking and Education", "Care coordination",
+#        "Availability", "Family Centerdness", "PC Networking",
+#        "Education and training", "Certification"]
 
 
 
