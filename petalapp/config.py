d7f33cd0529307937fdde48ceea28ae8c6ebec58
Drew Verlee <Drew.verlee@gmail.com>
2012-11-28 13:46:51 -0500

    trying to build database

--- petalapp/config.py
+++ petalapp/config.py
@@ -2,23 +2,29 @@
 File: config.py
 Date: 2012-11
 Author: Drew Verlee
-Description: configuration setup to handle aws,heroku, local machine,database,
-etc...
+Description: configuration setup to handle aws,heroku, local machine,
+database, etc...
 '''
 
 
 import os
 
+# flask
 PORT = int(os.environ.get("PORT", 5000))
 basedir = str(os.path.abspath(os.path.dirname(__file__)))
 SECRET_KEY = str(os.environ.get("APP_SECRET_KEY"))
 DEBUG = str(os.environ.get("DEBUG"))
-SQLALCHEMY_DATABASE_URI = str(os.environ.get("DATABASE_URL"))
-AWS_ACCESS_KEY_ID = str(os.environ.get("AWS_ACCESS_KEY_ID"))
-S3_BUCKET = str(os.environ.get("S3_BUCKET"))
-AWS_SECRET_ACCESS_KEY = str(os.environ.get("AWS_SECRET_ACCESS_KEY"))
-TESTING = os.environ.get("TESTING", False)
-S3_UPLOAD_DIRECTORY = str(os.environ.get("S3_UPLOAD_DIRECTORY"))
 ALLOWED_EXTENSIONS = str(os.environ.get("ALLOWED_EXTENSIONS"))
 CSRF_ENABLED = True
-SQLALCHEMY_MIGRATE_REPO = str(os.path.join(basedir, 'db_repository'))
+TESTING = os.environ.get("TESTING", False)
+
+#database, TODO add local db?
+SQLALCHEMY_DATABASE_URI = str(os.environ.get("DATABASE_URL"))
+SQLALCHEMY_MIGRATE_REPO = str(os.path.join(basedir, 'database/db_repository'))
+
+# s3
+AWS_ACCESS_KEY_ID = str(os.environ.get("AWS_ACCESS_KEY_ID"))
+AWS_SECRET_ACCESS_KEY = str(os.environ.get("AWS_SECRET_ACCESS_KEY"))
+S3_BUCKET = str(os.environ.get("S3_BUCKET"))
+S3_UPLOAD_DIRECTORY = str(os.environ.get("S3_UPLOAD_DIRECTORY"))
+
--- petalapp/database/db_create.py
+++ petalapp/database/db_create.py
@@ -1,6 +1,6 @@
 from migrate.versioning import api
-from config import SQLALCHEMY_DATABASE_URI
-from config import SQLALCHEMY_MIGRATE_REPO
+from petalapp.config import SQLALCHEMY_DATABASE_URI
+from petalapp.config import SQLALCHEMY_MIGRATE_REPO
 from petalapp import db
 import os.path
 db.create_all()
