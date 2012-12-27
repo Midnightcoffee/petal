19c35d6b0b2cd9abe85f4551f270dab755e82880
Drew Verlee <Drew.verlee@gmail.com>
2012-12-26 23:39:25 -0500

    setup_app -> init_app

--- petalapp/database/db_test_many_create.py
+++ petalapp/database/db_test_many_create.py
@@ -5,9 +5,10 @@ Author: Drew Verlee
 Description: functions to build a many-to-many relationship
 '''
 
+db.session.drop_all()
 from models import db, User, Hospital, Data
 
-db.session.drop_all()
+
 data_test_1 = Data(1)
 user_test_1 = User("test_user_nickname")
 hospital_test_1 = Hospital("test_hospital_1")
@@ -17,6 +18,20 @@ for database_entry in [data_test_1, user_test_1, hospital_test_1]:
 
 db.session.commit()
 
+example = User.query.get(1)
+print(example)
+
+users = Users.query.all()
+for u in users:
+    db.session.delete(u)
+
+hospitals = Hospital.query.all()
+for h in hospitals:
+    db.session.delete(h)
+
+data = Data.query.all()
+for d in data:
+    db.session.delete(d)
 
 
 
