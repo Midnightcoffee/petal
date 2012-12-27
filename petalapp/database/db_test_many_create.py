e7d22882f85d5577ce58d6534c7e085a1bf54b78
Drew Verlee <Drew.verlee@gmail.com>
2012-12-27 11:59:26 -0500

    testing database

--- petalapp/database/db_test_many_create.py
+++ petalapp/database/db_test_many_create.py
@@ -14,6 +14,8 @@ data_test_1 = Data(1)
 user_test_1 = User("test_user_nickname","user_email",ROLE_USER)
 hospital_test_1 = Hospital("test_hospital_1")
 
+
+# add them to the database
 for database_entry in [data_test_1, user_test_1, hospital_test_1]:
     db.session.add(database_entry)
 
@@ -23,17 +25,22 @@ example = User.query.get(1)
 print(example)
 
 users = User.query.all()
+print("********************** users **************")
 for u in users:
+    print(u)
     db.session.delete(u)
 
 hospitals = Hospital.query.all()
+print("********** hostpitals ***************")
 for h in hospitals:
+    print(h)
     db.session.delete(h)
 
 data = Data.query.all()
+print("******************* data **************")
 for d in data:
+    print(d)
     db.session.delete(d)
 
 
-
-
+db.session.commit()
