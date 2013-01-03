6062785f8f84c31aa4556a199c3dae68e7167106
Drew Verlee <Drew.verlee@gmail.com>
2013-01-03 00:32:17 -0500

    petalapp/aws/tools.py changes made to download_s3 method needs work

--- petalapp/aws/tools.py
+++ petalapp/aws/tools.py
@@ -7,10 +7,13 @@ Description: contains upload_s3_chart and download_s3_chart
 
 import boto
 from boto.s3.key import Key
+from boto.s3.connection import S3Connection
 from petalapp import app
+from petal.config import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY,S3_BUCKET
 
 from petalapp.graphing_tools.graph import plotpolar
 
+#TODO refactor see aws imports
 #TODO separate out functions, add try except
 def upload_s3(destination_filename, data, acl="public-read"):
     """upload_s3 uploads a string to s3"""
@@ -26,13 +29,17 @@ def upload_s3(destination_filename, data, acl="public-read"):
     sml.set_acl(acl)
 
 #TODO returning key, need a way to connect it to rest of code/add filename?
+#def download_s3():
+#    """download_s3 downloads from s3"""
+#    conn = boto.connect_s3(app.config["AWS_ACCESS_KEY_ID"],
+#            app.config["AWS_SECRET_ACCESS_KEY"])
+#    b = conn.get_bucket(app.config["S3_BUCKET"])
+#
+#    k = Key(b)
+#    return k
+#
+
 def download_s3():
-    """download_s3 downloads from s3"""
-    conn = boto.connect_s3(app.config["AWS_ACCESS_KEY_ID"],
-            app.config["AWS_SECRET_ACCESS_KEY"])
-    b = conn.get_bucket(app.config["S3_BUCKET"])
-
-    k = Key(b)
-    return k
-
-
+    s3 = S3Connection(AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY, is_secure=False)
+    url = s3.generate_url(60, 'GET', bucket=S3_BUCKET, key="Your_FILE_KEY", force_http=True) #FIXME Your_FILE_KEY figure out how to get file
+    return url #FIXME remember to filter url
