41dde32b003eb9e2d4b7e5a9484e55516ed42a87
Drew Verlee <Drew.verlee@gmail.com>
2013-01-08 14:20:11 -0500

    added exluded

--- petalapp/__init__.py
+++ petalapp/__init__.py
@@ -51,7 +51,7 @@ browserid.init_app(app)
 
 
 #from flask.ext.sslify import SSLify
-sslify = flask_sslify.SSLify(app)
+sslify = flask_sslify.SSLify(app, exluded =['login','logout','favicon'])
 
 
 #note that we are only enabling the emails when we run without debugging.
