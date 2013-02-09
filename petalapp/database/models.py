093142dfc316e4ed6af71114e167fa7c63133761
Drew Verlee <Drew.verlee@gmail.com>
2013-02-08 23:49:45 -0500

    cleaned up models a bit added data back in, many to many, org-uss,etc..

--- petalapp/database/models.py
+++ petalapp/database/models.py
@@ -9,10 +9,10 @@ class User(db.Model):
     """User has a many-to-many relationship with Organization"""
 
     id = db.Column(db.Integer, primary_key=True)
-    survey_comments = db.relationship('SurveyComment', backref='user', lazy='dynamic')
-    answers = db.relationship('Answer', backref='user',lazy='dynamic')
     email = db.Column(db.String(150), unique=True)
     role = db.Column(db.SmallInteger, default=ROLE_VIEWER)
+    answers = db.relationship('Answer', backref='user',lazy='dynamic')
+    survey_comments = db.relationship('SurveyComment', backref='user', lazy='dynamic')
     user_survey_sections = db.relationship('UserSurveySection', backref='user', lazy='dynamic')
 
     def __init__(self, email, role=ROLE_VIEWER): #FIXME: redundant
@@ -44,17 +44,19 @@ class User(db.Model):
             db.session.commit()
         return user
 
+survey_headers = db.Table('survey_headers',
+        db.Column('survey_header_id', db.Integer, db.ForeignKey('survey_headers.id')),
+        db.Column('organizations_id', db.Integer, db.ForeignKey('organization')))
 
 class Organization(db.Model):
     """
     Organization has a many-to-one relationship with Market
     Organization has a  one-to-many relationship with survey_headers
-
     """
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-    survey_headers = db.relationship('SurveyHeader', backref='organization', lazy='dynamic')
+    survey_headers = db.relationship('SurveyHeader', secondary=survey_headers,
+        backref=db.backref('organizations', lazy='dynamic'))
     market_id = db.Column(db.Integer, db.ForeignKey('market.id'))
 
     def __init__(self, name=''):
@@ -62,6 +64,7 @@ class Organization(db.Model):
 
     def __repr__(self):
         return '<Name: %r>' % self.name
+
 
 
 class Market(db.Model):
@@ -88,13 +91,14 @@ class SurveyHeader(db.Model):
     Survey_header has a one-to-many relationship with survey_comments
     """
     id = db.Column(db.Integer, primary_key=True)
-    organization_id = db.Column(db.Integer, db.ForeignKey('organization.id'))
-    survey_sections = db.relationship('SurveySection', backref='survey_header',lazy='dynamic')
-    survey_comments = db.relationship('SurveyComment', backref='survey_header',lazy='dynamic')
     name = db.Column(db.String(80))
-    instructions = db.Column(db.String(3000))
     other_info =  db.Column(db.String(250))
     time_period = db.Column(db.Integer)
+
+    survey_sections = db.relationship('SurveySection', backref='survey_header',lazy='dynamic')
+    survey_comments = db.relationship('SurveyComment', backref='survey_header',lazy='dynamic')
+    instructions = db.Column(db.String(3000))
+
 
 
     def __init__(self, time_period, name='',instructions='',other_info='', ):
@@ -114,10 +118,9 @@ class SurveyComment(db.Model):
     Survey_comments has a many-to-one relationship with Users
     """
     id = db.Column(db.Integer, primary_key=True)
+    comments = db.Column(db.String(3000))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     survey_header_id = db.Column(db.Integer, db.ForeignKey('survey_header.id'))
-    comments = db.Column(db.String(3000))
-
 
     def __init__(self, comments=''):
         self.comments = comments
@@ -133,17 +136,17 @@ class SurveySection(db.Model):
     Survey_section has a one-to-many relationship with Question
     """
     id = db.Column(db.Integer, primary_key=True)
-    survey_header_id = db.Column(db.Integer, db.ForeignKey('survey_header.id'))
-    parent_id = db.Column(db.Integer, db.ForeignKey('survey_section.id'))
-    user_survey_sections = db.relationship('UserSurveySection', backref="survey_section",
-            lazy='dynamic')
-    questions = db.relationship('Question', backref='survey_section', lazy='dynamic')
     order = db.Column(db.Integer)
     name = db.Column(db.String(100))
     subheading = db.Column(db.String(1200))
     required_yn = db.Column(db.Boolean)
     time_period = db.Column(db.String(100))
+    user_survey_sections = db.relationship('UserSurveySection', backref="survey_section",
+            lazy='dynamic')
+    questions = db.relationship('Question', backref='survey_section', lazy='dynamic')
     children = db.relationship('SurveySection', lazy='dynamic')
+    survey_header_id = db.Column(db.Integer, db.ForeignKey('survey_header.id'))
+    parent_id = db.Column(db.Integer, db.ForeignKey('survey_section.id'))
 
 
 
@@ -164,9 +167,12 @@ class UserSurveySection(db.Model):
     User_survey_section has a many-to-many relationship with User
     """
     id = db.Column(db.Integer, primary_key=True)
-    survey_section_id = db.Column(db.Integer, db.ForeignKey('survey_section.id'))
     completed_date = db.Column(db.DateTime)
+    answers = db.relationship('Answer', backref='user_survey_section', 
+            lazy='dynamic')
+    survey_section_id = db.Column(db.Integer, db.ForeignKey('survey_section.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 
     def __init__(self, completed_date=None):
         self.completed_date = completed_date
@@ -188,9 +194,7 @@ class Answer(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     question_option_id = db.Column(db.Integer, db.ForeignKey('question_option.id'))
     unit_of_measurement_id = db.Column(db.Integer, db.ForeignKey('unit_of_measurement.id'))
-    #organization_id = db.Column(db.Integer, db.ForeignKey('organization.id'))
-    #question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
-    #survey_id = db.Column(db.Integer, db.ForeignKey('survey.id'))
+
 
     def __init__(self, numeric=0):
         self.numeric = numeric
@@ -269,10 +273,11 @@ class Question(db.Model):
     Question has a one-to-many relationship with Question_option
     """
     id = db.Column(db.Integer, primary_key=True)
+    question_options = db.relationship('QuestionOption', backref='question',lazy='dynamic')
     survey_section_id = db.Column(db.Integer, db.ForeignKey('survey_section.id'))
     inputtype_id = db.Column(db.Integer, db.ForeignKey('input_type.id'))
     option_group_id = db.Column(db.Integer, db.ForeignKey('option_group.id'))
-    question_options = db.relationship('QuestionOption', backref='question',lazy='dynamic')
+
 
     #TODO old look over
     name = db.Column(db.String(500))
@@ -316,6 +321,89 @@ class InputType(db.Model):
         return '<Input_type: %r>' % self.input_type
 
 
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    standard_form = db.Column(db.Integer)
+    marketing_education = db.Column(db.Integer)
+    record_availability = db.Column(db.Integer)
+    family_centerdness = db.Column(db.Integer)
+    pc_networking = db.Column(db.Integer)
+    education_and_training = db.Column(db.Integer)
+    team_funding = db.Column(db.Integer)
+    coverage = db.Column(db.Integer)
+    pc_for_expired_pts = db.Column(db.Integer)
+    hospital_pc_screening  = db.Column(db.Integer)
+    pc_follow_up = db.Column(db.Integer)
+    post_discharge_services = db.Column(db.Integer)
+    bereavement_contacts = db.Column(db.Integer)
+    certification = db.Column(db.Integer)
+    team_wellness = db.Column(db.Integer)
+    care_coordination = db.Column(db.Integer)
 
+    timestamp = db.Column(db.DateTime)
+
+    hospital_id = db.Column(db.Integer, db.ForeignKey('hospital.id'))
+
+    def __init__(self, standard_form=0,
+            marketing_education=0, record_availability=0, family_centerdness=0,
+        pc_networking=0, education_and_training=0, team_funding=0,
+        coverage=0, pc_for_expired_pts=0, hospital_pc_screening=0,
+        pc_follow_up=0, post_discharge_services=0, bereavement_contacts=0,
+        certification=0, team_wellness=0, care_coordination=0, timestamp=datetime.utcnow()):
+
+            self.standard_form = standard_form
+            self.marketing_education =  marketing_education
+            self.record_availability = record_availability
+            self.family_centerdness =  family_centerdness
+            self.pc_networking =  pc_networking
+            self.education_and_training =  education_and_training
+            self.team_funding =  team_funding
+            self.coverage =  coverage
+            self.pc_for_expired_pts =  pc_for_expired_pts
+            self.hospital_pc_screening  =  hospital_pc_screening
+            self.pc_follow_up =  pc_follow_up
+            self.post_discharge_services =  post_discharge_services
+            self.bereavement_contacts =  bereavement_contacts
+            self.certification =  certification
+            self.team_wellness =  team_wellness
+            self.care_coordination = care_coordination
+            self.timestamp = timestamp
+
+    def __repr__(self):
+        return """
+    <standard_form : %r>\n
+    <marketing_education : %r>\n
+    <record_availability : %r>\n
+    <family_centerdness : %r>\n
+    <pc_networking : %r>\n
+    <education_and_training : %r>\n
+    <team_funding : %r>\n
+    <coverage : %r>\n
+    <pc_for_expired_pts : %r>\n
+    <hospital_pc_screening  : %r>\n
+    <pc_follow_up : %r>\n
+    <post_discharge_services : %r>\n
+    <bereavement_contacts : %r>\n
+    <certification : %r>\n
+    <team_wellness : %r>\n
+    <care_coordination : %r>\n
+    <datetime_utc  : %r>""" % (
+    self.standard_form,
+    self.marketing_education,
+    self.record_availability,
+    self.family_centerdness,
+    self.pc_networking,
+    self.education_and_training,
+    self.team_funding,
+    self.coverage,
+    self.pc_for_expired_pts,
+    self.hospital_pc_screening ,
+    self.pc_follow_up,
+    self.post_discharge_services,
+    self.bereavement_contacts,
+    self.certification,
+    self.team_wellness,
+    self.care_coordination,
+    self.timestamp)
 
 
